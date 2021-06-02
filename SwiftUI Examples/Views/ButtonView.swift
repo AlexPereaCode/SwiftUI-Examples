19f6fe2437bb4ee985985ffe0312f3664421c0c4
Alex Perea <alexpereacode@gmail.com>
2021-06-02 21:05:27 +0200

    Added button examples

--- SwiftUI Examples/Views/ButtonView.swift
+++ SwiftUI Examples/Views/ButtonView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct ButtonView: View {
+    
+    @State var backgroundColor: Color = .green
+    @State var backgroundColor2: Color = .blue
+    
     var body: some View {
-        Text("ButtonView")
+        VStack {
+            Button(action: {
+                backgroundColor = backgroundColor == .green ? .red : .green
+            }, label: {
+                Text("Press Me")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+                    .background(backgroundColor)
+                    .cornerRadius(8)
+                    .shadow(radius: 8)
+            })
+            .padding(30)
+            
+            Button(action: {
+                backgroundColor2 = backgroundColor2 == .blue ? .red : .blue
+            }, label: {
+                Circle()
+                    .fill(backgroundColor2)
+                    .frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .shadow(radius: 8)
+                    .overlay(
+                        Image(systemName: "calendar")
+                            .foregroundColor(.white)
+                            .font(.system(size: 70))
+                    )
+            })
+        }
     }
 }
 
